f14c2ca21a2489bd654919362df3626bf9cd043b
mikexine <mikexine@gmail.com>
2015-07-13 03:44:07 -0400

    update

--- script.py
+++ script.py
@@ -9,16 +9,16 @@ apisecret = 'INSERT_SECRET'
 rock = PyRock(apikey, apisecret)
 price = rock.Ticker('btceur')['last']
 
-font = ImageFont.truetype('./Nobile-Medium.ttf', 26)
+font = ImageFont.truetype('/home/bot/TheRockPNG/Nobile-Medium.ttf', 26)
 img = Image.new('RGBA', (280, 50), (255, 255, 255, 0))
 draw = ImageDraw.Draw(img)
 draw.text((0, 0), '{0:.2f} EUR/BTC'.format(price).replace('.', ','), (0, 0, 0),font=font)
 draw = ImageDraw.Draw(img)
-img.save('./pricexbteur.png')
+img.save('/home/bot/TheRockPNG/pricexbteur.png')
 
-a = Image.open("./backgroundTR.png")
-b = Image.open('./pricexbteur.png')
+a = Image.open("/home/bot/TheRockPNG/backgroundTR.png")
+b = Image.open('/home/bot/TheRockPNG/pricexbteur.png')
 a.paste(b, (70, 12), b)
-a.save('/var/www/trtlast.png')
+a.save('/var/www/html/trtlast.png')
 
 
